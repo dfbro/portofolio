f2abd8c4f7b7c8f6abda2c5768d75ac7d0ea2802
Daffa Arzany <daffafnea@gmail.com>
2025-12-15 12:11:40 +0000

    just use experimental: { serverActions: { allowedOrigins:

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,7 +1,14 @@
 import type {NextConfig} from 'next';
 
+const appHostname = process.env.APP_HOSTNAME || 'localhost:9002';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [appHostname],
+    },
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
