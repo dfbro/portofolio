42ef71b00d445958fdf8210375cdda679d5c7387
Daffa Arzany <daffafnea@gmail.com>
2025-12-15 09:03:09 +0000

    make the design more modern with material UI + dark theme and Blue-Neon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['var(--font-manrope)', 'sans-serif'],
+        headline: ['var(--font-manrope)', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
