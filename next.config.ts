1eee19056dd7882b01775279ee083335ad55dd58
Daffa Arzany <daffafnea@gmail.com>
2025-12-15 12:10:48 +0000

    not image hostnames, i said allowedorigins

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
 import type {NextConfig} from 'next';
 
 const appHostname = process.env.APP_HOSTNAME || 'localhost';
+const protocol = process.env.APP_SSL === 'true' ? 'https' : 'http';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: `${protocol}://${appHostname}` },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -12,14 +25,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-       {
-        protocol: 'https',
-        hostname: appHostname.split(':')[0],
-      },
-      {
-        protocol: 'http',
-        hostname: appHostname.split(':')[0],
-      },
       {
         protocol: 'https',
         hostname: '**',
